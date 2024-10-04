f83512b9795bcd4261ada6feadd11bbb2f099735
Vatsal <patelvatsal79715@gmail.com>
2024-10-04 22:51:21 +0530

    add user and video model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,10 +1,12 @@
 import mongoose , {Schema}  from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt"
 
 const userSchema = new Schema({
     username: {
         type: String,
         required: true,
-        unique: true,
+        unique: true, 
         lowercase: true,
         trime: true,
         index: true
@@ -47,5 +49,42 @@ const userSchema = new Schema({
     timestamps: true
 })
 
+userSchema.pre("save", async function(next){
+    if(!this.ismodified("password")) return next()
 
+    this.password = bcrypt.hash(this.password ,10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect =  async function(password){
+
+ return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    jwt.sign(
+        {
+            _id: this._id,
+            name: this.name,
+            fullname: this.fullname,
+            email: this.email
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 export const User =  mongoose.model("User",userSchema)
